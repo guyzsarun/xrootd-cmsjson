807ccbc4ade21bd2174a66b793db642ca1734e95
root <47900195+4quarks@users.noreply.github.com>
2022-07-27 10:52:17 +0200

    add inheritance Name2Name

--- main.cc
+++ main.cc
@@ -10,13 +10,14 @@ int main (int, const char** argv)
    //const char* lfn = "/store/test/14c5c58e-00c2-4660-bf90-a963b86388e1.root";
    int blen = 4096;
    char* buff = (char*) malloc(blen);
-   char rf[] = "/root/xrootd-cmsjson/storage_test.json?volume=Test_dCache&protocol=WebDAV";
+   char rf[] = "file:/root/xrootd-cmsjson/storage_test.json?volume=Test_dCache&protocol=WebDAV";
    XrdSysLogger myLogger;
    XrdSysError eDest(&myLogger, "tfc_");
    eDest.Say("TFC Module");
-   XrdCmsJson::PathTranslation cmsJson(&eDest, rf);
+   XrdCmsJson::PathTranslation *cmsJson = new XrdCmsJson::PathTranslation (&eDest, rf);
    const char* lfn = argv[1];
-   cmsJson.lfn2pfn(lfn, buff, blen);
+   cmsJson->lfn2pfn(lfn, buff, blen);
    std::cout << buff << std::endl;
    return 0;
 }
+
